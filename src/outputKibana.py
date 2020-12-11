f33f55fe9c129f2836bc06568f16819e5845df86
marlene <maan@gomspace.com>
2020-12-11 10:45:55 +0100

    upgrade on visualsand kibana interaction

--- src/outputKibana.py
+++ src/outputKibana.py
@@ -2,37 +2,100 @@
 
 """ Call ElasticSearch AWS """
 
+import os
+import glob
+from pathlib import Path
+import json
+import csv
+from threading import Thread
+import subprocess
+
 #Import ES library
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 import boto3
 
-#Import ORCHESTRATOR file managment
+#Establish Paths to Folders
+import simImports
 
-host = 'https://search-hoopsim-xebvo4edd36kgunyxhaxct2tqi.eu-central-1.es.amazonaws.com' # For example, my-test-domain.us-east-1.es.amazonaws.com
-region = 'eu-central-1' # e.g. us-west-1
+# #Import ORCHESTRATOR file managment
+from orchestratorFile import OrchestratorFile
 
-service = 'es'
-credentials = boto3.Session().get_credentials()
-awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+class AWSKibanaHandler(OrchestratorFile):
+    
+    def initializeService(self) -> None:
+        """initializeService
+            Initalzie AWS service
+        """
+        #Define AWS host
+        self.host = 'https://search-hoopsim-xebvo4edd36kgunyxhaxct2tqi.eu-central-1.es.amazonaws.com'
+        region = 'eu-central-1'
+        
+        #Define the service
+        service = 'es'
+        credentials = boto3.Session().get_credentials()
+        awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+        
+        try:
+            self.es = Elasticsearch( hosts = [self.host], #{'host': host, 'port': 443}
+                                        http_auth = awsauth,
+                                        use_ssl = True,
+                                        verify_certs = True,
+                                        connection_class = RequestsHttpConnection
+                                    )
 
-es = Elasticsearch(
-    hosts = [{'host': host, 'port': 443}],
-    http_auth = awsauth,
-    use_ssl = True,
-    verify_certs = True,
-    connection_class = RequestsHttpConnection
-)
 
-document = {
-    "title": "Moneyball",
-    "director": "Bennett Miller",
-    "year": "2011"
-}
+        except:
+            print('Connection with Server was not established')
 
-es.index(index="movies", doc_type="_doc", id="5", body=document)
+    def createIndex(self) ->None:
+        '''createIndex
+            Create Index Pattern in ElasticSearch
+        '''
+        print(dir(self.es.index))
 
-print(es.get(index="movies", doc_type="_doc", id="5"))
+        indexESName = 'hoopsim'
+        indexFile   = 'index_hoopsim.json'
 
-# -*- coding: utf-8 -*-
+        try:
+            curl = f'curl -vs -X PUT {self.host}/{indexESName}* -H Content-Type: application/json -d @{indexFile}'
+            os.system(curl)
+            print('index pattern is created')
+        except:
+            print('index pattern was already created')
 
+    
+    def main(self, outputPath : str, scenarioFile: str) -> bool:
+        """ The AWSKibanaHandler will perform following tasks:
+            - check if the output folder contains results
+            - call AWS service
+            - convert .out files into ES compatible dictionary and send to AWS Kibana
+            
+            return False if failed
+        """
+        
+        #Initialize AWS service
+        self.initializeService()
+        self.createIndex()
+
+        #Get scenario output name
+        scenarioData = self.getScenarioData(scenarioFile)
+        scenarioId = scenarioData['simulation']['id']
+        outputPath = outputPath + 'sim' + scenarioId #outputPath + "/" + scenarioId
+
+        ESFilesLocation = outputPath + '/ElasticSearch/'
+        print('ESFilesLocation', ESFilesLocation)
+
+        for ESFile in os.listdir(ESFilesLocation):
+            with open(ESFilesLocation + ESFile, "r") as jsonFile:
+                EScontent = json.load(jsonFile)
+                # EScontent = csv.reader(jsonFile)
+                # print(EScontent)
+                # input()
+                #Send to AWS ES service each report
+                indexId = ESFile.lower()
+                indexId = indexId.replace('.json', '')
+                # indexId = indexId.replace('.out', '')
+                for ESblock in EScontent:
+                    self.es.index(index=f"hoopsim", doc_type="_doc", id=indexId, body=ESblock)
+                    print('\n')
--- src/serviceHandler.py
+++ src/serviceHandler.py
@@ -3,72 +3,17 @@
 import subprocess
 import http.server
 from pathlib import Path
-import sys
+from threading import Thread
 
 #This module defines a number of utilities for use by CGI scripts written in Python.
 import cgi
-from os import curdir, sep, chdir, getcwd
 
-import os
+#Establish Paths to Folders
+import simImports
 
-
-################################################################################
-#Import working folders
-import os
-
-from threading import Thread
-import subprocess
-import gc
-
-import webbrowser
-
-###############################################################################
-# ESTABLISH PATHS TO FOLDERS
-###############################################################################
-
-
-simulationPlatformPath = Path(__file__).parent.joinpath('../../mcop-simulation-platform').resolve(True)
-federatesPath = Path(__file__).parent.joinpath('../../mcop-simulation-federates').resolve(True)
-modelsPath = Path(__file__).parent.joinpath('../../mcop-simulation-models').resolve(True)
-
-relativePath = [
-    str(federatesPath.joinpath('sim_federate/rti_connections')),
-    str(federatesPath.joinpath('sim_federate/src')),
-    str(federatesPath.joinpath('sim_orbit/rti_connections')),
-    str(federatesPath.joinpath('sim_orbit/src')),
-    str(federatesPath.joinpath('sim_orchestrator/rti_connections')),
-    str(federatesPath.joinpath('sim_orchestrator/src')),
-    str(federatesPath.joinpath('sim_shared/src')),
-    str(federatesPath.joinpath('util')),
-    str(federatesPath.joinpath('libs')),
-
-    str(modelsPath.joinpath('sim_batt/src')),
-    str(modelsPath.joinpath('sim_payload/src')),
-    str(modelsPath.joinpath('sim_pcdu/src')),
-    str(modelsPath.joinpath('sim_receiver/src')),
-    str(modelsPath.joinpath('sim_shared/src')),
-    str(modelsPath.joinpath('sim_solararr/src')),
-    str(modelsPath.joinpath('sim_storageunit/src')),
-    str(modelsPath.joinpath('sim_transmitter/src')),
-    str(modelsPath.joinpath('sim_transceiver/src')),
-]
-paths = ":".join(relativePath)
-os.environ["PYTHONPATH"] = paths + ":" + ("" if os.environ.get("PYTHONPATH") == None else os.environ.get("PYTHONPATH"))
-
-#Import ORCHESTRATOR file managment
-#(the paths established above are not immediate to take action)
-sys.path.insert(0, '../../mcop-simulation-federates/sim_orchestrator/src')
-sys.path.insert(0, '../../mcop-simulation-federates/sim_shared/src')
-sys.path.insert(0, '../../mcop-simulation-models/sim_batt/src')
-sys.path.insert(0, '../../mcop-simulation-models/sim_payload/src')
-sys.path.insert(0, '../../mcop-simulation-models/sim_pcdu/src')
-sys.path.insert(0, '../../mcop-simulation-models/sim_receiver/src')
-sys.path.insert(0, '../../mcop-simulation-models/sim_shared/src')
-sys.path.insert(0, '../../mcop-simulation-models/sim_solararr/src')
-sys.path.insert(0, '../../mcop-simulation-models/sim_storageunit/src')
-sys.path.insert(0, '../../mcop-simulation-models/sim_transmitter/src')
-sys.path.insert(0, '../../mcop-simulation-models/sim_transceiver/src')
+#Import the modules needed in this script
 from orchestratorFile import OrchestratorFile
+from outputKibana import AWSKibanaHandler
 
 
 #################################################################################
@@ -80,6 +25,7 @@ class ServerHandler(http.server.BaseHTTPRequestHandler, OrchestratorFile): #Orch
     This class will handle any incoming request from the browser 
     It derives from BaseHTTPRequestHandler
     '''
+
 	#Handler for the GET requests
     def do_GET(self):
         self.path = ("webGateway.html")
@@ -175,6 +121,7 @@ class ServerHandler(http.server.BaseHTTPRequestHandler, OrchestratorFile): #Orch
             #Open simulation-scenario-bundle in a new tab
             elif "openScenarioFile" in form.keys():
                 # filename = '/mnt/c/users/maan/projects/mcop-simulation-federates/input/simulation-scenario-bundle.json'
+                # import webbrowser
                 # webbrowser.open('file://' + os.path.realpath(filename))
                 pass
                 
@@ -182,8 +129,13 @@ class ServerHandler(http.server.BaseHTTPRequestHandler, OrchestratorFile): #Orch
                 
             #Call the output in KIBANA
             elif "kibanaOutput" in form.keys():
-                pass
-            # util.generatorRandomScenario
+                # Create Class
+                awsES = AWSKibanaHandler()
+                # Call main
+                awsES.main(outputFolder, scenarioFile)
+
+            # else:
+                # util.generatorRandomScenario
 
             else:
                 self.assassin.start()
